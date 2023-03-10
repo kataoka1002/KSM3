5770023aeb06678317fad9f13aa9166516fff079
村上和志 <CA01972045@st.kawahara.ac.jp>
2023-03-10 18:10:35 +0900

    背景実装_やり直し

---
+++ k2EngineLow-main/k2EngineLow-main/KSM3/Game/BackGround.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include "BackGround.h"
+
+BackGround::BackGround()
+{
+
+}
+
+BackGround::~BackGround()
+{
+	
+}
+
+bool BackGround::Start()
+{
+	//モデルを読み込む。
+	modelRender.Init("Assets/modelData//stage.tkm");
+	//モデルを更新する。
+	modelRender.Update();
+	//静的物理オブジェクトを作成。
+	physicsStaticObject.CreateFromModel(modelRender.GetModel(), modelRender.GetModel().GetWorldMatrix());
+	// 当たり判定を有効化する。
+	//PhysicsWorld::GetInstance()->EnableDrawDebugWireFrame();
+	return true;
+}
+
+void BackGround::Update()
+{
+	//モデルの更新処理。
+	modelRender.Update();
+}
+
+void BackGround::Render(RenderContext& rc)
+{
+	modelRender.Draw(rc);
+}
