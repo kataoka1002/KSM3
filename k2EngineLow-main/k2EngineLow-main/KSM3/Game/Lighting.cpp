ea26840fb7d0addcce239bec483bb8c09295d787
Kataoka <ca01972139@st.kawahara.ac.jp>
2023-07-04 17:59:09 +0900

    ライトの変数名を変えた

--- k2EngineLow-main/k2EngineLow-main/KSM3/Game/Lighting.cpp
+++ k2EngineLow-main/k2EngineLow-main/KSM3/Game/Lighting.cpp
@@ -1,28 +1,20 @@
 #include "stdafx.h"
 #include "Lighting.h"
 
-Lighting::Lighting() {
+Lighting::Lighting()
+{
+
 	//ライト方向の正規化
-	Light_Direction.Normalize();
-	spLightDirection.Normalize();
+	m_dirLightDirection.Normalize();
+	m_spLightDirection.Normalize();
+
 
 	//ディレクションライトの設定
-	g_renderingEngine->SetDirectionLight(Light_Direction, Light_Color);
+	g_renderingEngine->SetDirectionLight(m_dirLightDirection, m_dirLightColor);
+
+
 	//カスタムエリアのライトの設定
-	g_renderingEngine->SetSpotLight(0, {10000.0f,70.0f,-100.0f}, 1000.0f, spLightColor, spLightDirection, 45.0f);
-}
-
-bool Lighting::Start()
-{
-	return true;
-}
-
-Lighting::~Lighting() {
-
-}
-
-void Lighting::Update()
-{
+	g_renderingEngine->SetSpotLight(0, m_spLightPosition, m_spLightRange, m_spLightColor, m_spLightDirection, m_spLightAngle);
 
 }
 
