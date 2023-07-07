068093fd8e6da67d5d1ce3671a8b4e748079b278
Kataoka <ca01972139@st.kawahara.ac.jp>
2023-07-07 11:52:31 +0900

    セイバーをprivateにした

--- k2EngineLow-main/k2EngineLow-main/KSM3/Game/Boss_Saber.h
+++ k2EngineLow-main/k2EngineLow-main/KSM3/Game/Boss_Saber.h
@@ -1,52 +1,65 @@
 #pragma once
-#define HP 500.0f
 
 class Player;
 class Boss;
-class Boss_Cannon_attack;
-class Drop_item;
 class Game;
 
-class Boss_Saber :public IGameObject
+class Boss_Saber : public IGameObject
 {
 public:
 	Boss_Saber();
 	~Boss_Saber();
-
+	bool Start();
 	void Setup();
 	void Update();
 	void Move();
-	void PlayerSearch();
 	void Render(RenderContext& rc);
 
-	Player* m_player;
-	Boss* m_boss;
-	Boss_Cannon_attack* b_boss_weapons;
-	Drop_item* m_dropItem;
-	Game* m_game;
-	EffectEmitter* m_weaponEffect = nullptr;
+	Vector3 GetPosirion()
+	{
+		return m_position;
+	}
 
-	//CharacterController boss_riser_characterContller;
+	Quaternion GetRotation()
+	{
+		return m_rotation;
+	}
+
+	void ApplyDamage(float damage)
+	{
+		m_HP -= damage;
+	}
+
+	/// <summary>
+	/// セイバーの大きさ減少
+	/// </summary>
+	/// <param name="amount">減少量</param>
+	void DecreaseScale(float amount)
+	{
+		m_scale -= amount;
+	}
+
+	/// <summary>
+	/// セイバーの大きさを設定
+	/// </summary>
+	void SetSaberScale()
+	{
+		m_saberModel.SetScale(m_scale);
+	}
+
+
+private:
+	Player* m_player = nullptr;
+	Boss* m_boss = nullptr;
+	Game* m_game = nullptr;
+
+	ModelRender m_saberModel;
 	CharacterController m_enemyCharacterController;	//エネミーキャラコン
 	Quaternion m_rotation;
-	Vector3 m_position;
-	Vector3 iranyatu;
+	Vector3 m_position = Vector3::Zero;
 	Vector3 m_forward = { 0.0f,0.0f,1.0f };
-	int iran = 0;
-	ModelRender boss_Cannon_Render;
-
-	//いる。絶対。
 	Vector3 m_localPosition = { 00.0f,00.0f,-1450.0f };
-
-	bool m_attackState = false;
-	bool m_attackOK = false;
-	bool notHituyou = false;//死んだかどうかのフラグ。
-	int m_setWeapon = 0;
-
-	int iraniran = 0;
-	int m_fastFlag = 0;
-
-	float connon_HP = HP;
-	float scale = 15.0f;
+	float m_HP = 500.0f;
+	float m_scale = 15.0f;
 };
 
